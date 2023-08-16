10e078c11629543a10c4e59af7daba6ce8135d5c
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-17 00:34:56 +0300

    Add files via upload

--- SCDElements.js
+++ SCDElements.js
@@ -1,17 +1,23 @@
 
 
-console.log(document.documentElement);
+//console.log(document.documentElement);
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.querySelectorAll('.wrapper'));
 
+const sections = document.querySelectorAll('.wrapper');
+const mainWrapper = document.querySelector('.mainWrapper');
+const body = document.querySelector('body');
+//console.log(sections);
 
+//console.log(document.getElementsByTagName('button'));
+//const buttons = document.getElementsByTagName('button');
+//console.log(buttons);
 
-
-
-
-
-
-
-
-
+const message = document.createElement('div');
+message.classList.add('message');
+message.textContent = 'Message Text';
+mainWrapper.append(message);
 
 
 
