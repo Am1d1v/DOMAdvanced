cb322bdbc123a655049a0ec386e9620b02900868
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-18 23:38:56 +0300

    Add files via upload

--- StylesAtributesClasses.js
+++ StylesAtributesClasses.js
@@ -1,15 +1,18 @@
 
 
 const mainWrapper = document.querySelector('.mainWrapper');
+const newWrapper = document.querySelector('.newWrapper');
 
 
-mainWrapper.style.backgroundColor = '090909';
+mainWrapper.style.backgroundColor = 'black';
 mainWrapper.style.width = "600px";
+
+newWrapper.style.height = '300px';
+console.log((mainWrapper.classList)[0]);
 
 console.log(getComputedStyle(mainWrapper).height);
 console.log(typeof(getComputedStyle(mainWrapper).height));
 mainWrapper.style.height = Number.parseFloat(getComputedStyle(mainWrapper).height) + 150 + 'px';
-
 
 
 
