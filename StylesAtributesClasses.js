5b525ba52bb0a6b73bd21cafc3e081710abccd72
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-18 23:20:38 +0300

    Add files via upload

--- StylesAtributesClasses.js
+++ StylesAtributesClasses.js
@@ -6,7 +6,7 @@ const mainWrapper = document.querySelector('.mainWrapper');
 mainWrapper.style.backgroundColor = '090909';
 mainWrapper.style.width = "600px";
 
-
+console.log(getComputedStyle(mainWrapper).height);
 
 
 
